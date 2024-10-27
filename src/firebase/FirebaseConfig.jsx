fdc3022d741fe5a163426180db95d26a184cb9b3
Narendra74a <151930659+Narendra74a@users.noreply.github.com>
2024-10-27 12:53:02 +0530

    Update FirebaseConfig.jsx

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -3,12 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3B8_72xK9QeYEa-nTeT3M8zsrPU0Q7kc",
-  authDomain: "myproject-supermall.firebaseapp.com",
-  projectId: "myproject-supermall",
-  storageBucket: "myproject-supermall.appspot.com",
-  messagingSenderId: "267854114503",
-  appId: "1:267854114503:web:1c5a6ad8efadc2e2d9d8e2"
+
 };
 
 // Initialize Firebase
